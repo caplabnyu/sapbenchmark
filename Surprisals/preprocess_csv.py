cee24d8e13f7b48af831c8938febe78514b57598
Suhas Arehalli <suhas@jhu.edu>
2022-05-19 20:14:42 -0400

    cleaning up surprisal code + COCA frequencies

--- Surprisals/preprocess_csv.py
+++ Surprisals/preprocess_csv.py
@@ -1,7 +1,16 @@
 import pandas
 
-main_name = "data/items_main"
+# Filenames to preprocess
+# TODO: Maybe automate gathering this, or put this in a shared config accessable by the scripts?
+main_name = "data/items_ClassicGP"
+other_names = ["data/items_Agreement", "data/items_AttachmentAmbiguity", "data/items_RelativeClause", "data/items_filler", "data/items_plaus"]
 
+def clean_sent(s):
+    """ Strip the sentences in the datafile of any weird escape characters so they match what participants saw """
+    if type(s) == str: return s.replace('\\', '')
+    return ""
+
+# Special processing for classic subset, since the ambig/unambig stimuli are in the same row
 main_df = pandas.read_csv(main_name + ".csv")
 main_df = main_df.melt(id_vars=["item", "condition", "disambPositionAmb", "disambPositionUnamb"], value_vars=["ambiguous", "unambiguous"], 
              var_name="ambiguity", value_name="Sentence")
@@ -10,16 +19,16 @@ main_df["disambPosition_0idx"] = [ambpos-1 if cond == "ambiguous" else unambpos-
                              in zip(main_df["disambPositionAmb"], main_df["disambPositionUnamb"], main_df["ambiguity"])]
 
 main_df = main_df[main_df["Sentence"] != ""] 
+main_df["Sentence"] = main_df["Sentence"].apply(clean_sent)
 
 main_df.to_csv(main_name + ".pivot.csv")
 
-
-other_names = ["data/items_agreement", "data/items_attach", "data/items_orc", "data/items_filler", "data/items_plaus"]
+# For others, filter out empty rows and get a 0idx'd version of disambPosition if it exists for that dataset
 
 for name in other_names:
-     
     df = pandas.read_csv(name + ".csv")
     df = df[df["Sentence"] != ""] 
+    df["Sentence"] = df["Sentence"].apply(clean_sent)
     if "disambPosition" in df:
         df["disambPosition_0idx"] = df["disambPosition"]
     df.to_csv(name + ".pivot.csv")
--- Surprisals/scripts/gpt_surps.sh
+++ Surprisals/scripts/gpt_surps.sh
@@ -1,13 +1,13 @@
 python preprocess_csv.py
 
-python get_gpt2_full.py --input data/items_main.pivot.csv --output data/gpt2/items_main.gpt2.csv --aligned
-python postprocess_csv.py --input data/gpt2/items_main.gpt2.csv --output data/gpt2/items_main.gpt2.post.csv
+python get_gpt2_full.py --input data/items_ClassicGP.pivot.csv --output data/gpt2/items_ClassicGP.gpt2.csv --aligned
+python postprocess_csv.py --input data/gpt2/items_ClassicGP.gpt2.csv --output data/gpt2/items_ClassicGP.gpt2.post.csv
 
-python get_gpt2_full.py --input data/items_agreement.pivot.csv --output data/gpt2/items_agreement.gpt2.csv --aligned
+python get_gpt2_full.py --input data/items_Agreement.pivot.csv --output data/gpt2/items_Agreement.gpt2.csv --aligned
 
-python get_gpt2_full.py --input data/items_attach.pivot.csv --output data/gpt2/items_attach.gpt2.csv --aligned
+python get_gpt2_full.py --input data/items_AttachmentAmbiguity.pivot.csv --output data/gpt2/items_AttachmentAmbiguity.gpt2.csv --aligned
 
-python get_gpt2_full.py --input data/items_orc.pivot.csv --output data/gpt2/items_orc.gpt2.csv --aligned
+python get_gpt2_full.py --input data/items_RelativeClause.pivot.csv --output data/gpt2/items_RelativeClause.gpt2.csv --aligned
 
 python get_gpt2_full.py --input data/items_filler.pivot.csv --output data/gpt2/items_filler.gpt2.csv --aligned
 
