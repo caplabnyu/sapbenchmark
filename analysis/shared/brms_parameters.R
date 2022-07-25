051dc8b8aee22d3eea279834b3dfa615a76d6a75
Grusha Prasad <grusha.prasad@gmail.com>
2022-07-25 15:10:25 -0400

    fixed bug in storing priors

--- analysis/shared/brms_parameters.R
+++ analysis/shared/brms_parameters.R
@@ -3,27 +3,27 @@ library(brms)
 
 # Prior 1 to 3 --> less informative to more informative
 
-if(prior_type == 'prior1'){
-  curr_prior = c(prior("normal(300,1000)", class = "Intercept"),
-                 prior("normal(0,150)", class = "b"),  
-                 prior("normal(0,200)", class = "sd"),    
-                 prior("normal(0,500)", class = "sigma"))
-}else if(prior_type == 'prior2'){
-  curr_prior = c(prior("normal(300,1000)", class = "Intercept"),
-                 prior("normal(0,100)", class = "b"),  
-                 prior("normal(0,200)", class = "sd"),
-                 prior("normal(0,500)", class = "sigma"))
-}else if(prior_type == 'prior3'){
-  curr_prior =c(prior("normal(300,1000)", class = "Intercept"),
-                prior("normal(0,100)", class = "b"),  
-                prior("normal(0,150)", class = "sd"),
-                prior("normal(0,300)", class = "sigma"))
-}else{
-  print('ENTER A VALID PRIOR')
-}
-
-
 get_brms_parameters <- function(prior_type){
+  
+  if(prior_type == 'prior1'){
+    curr_prior = c(prior("normal(300,1000)", class = "Intercept"),
+                   prior("normal(0,150)", class = "b"),  
+                   prior("normal(0,200)", class = "sd"),    
+                   prior("normal(0,500)", class = "sigma"))
+  }else if(prior_type == 'prior2'){
+    curr_prior = c(prior("normal(300,1000)", class = "Intercept"),
+                   prior("normal(0,100)", class = "b"),  
+                   prior("normal(0,200)", class = "sd"),
+                   prior("normal(0,500)", class = "sigma"))
+  }else if(prior_type == 'prior3'){
+    curr_prior =c(prior("normal(300,1000)", class = "Intercept"),
+                  prior("normal(0,100)", class = "b"),  
+                  prior("normal(0,150)", class = "sd"),
+                  prior("normal(0,300)", class = "sigma"))
+  }else{
+    print('ENTER A VALID PRIOR')
+  }
+  
   parms <- list(prior = curr_prior,
                 ncores = 4,
                 niters = 12000,
