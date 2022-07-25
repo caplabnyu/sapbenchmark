37dfbbf53e373817c366a88f5cf1b60b420b1b2e
Grusha Prasad <grusha.prasad@gmail.com>
2022-07-25 15:08:36 -0400

    fixed bug in storing priors

--- analysis/shared/brms_parameters.R
+++ analysis/shared/brms_parameters.R
@@ -1,27 +1,29 @@
 library(brms)
 
 
-# Prior 1 to 3 --> more informative to less informative
-prior1 <- c(prior("normal(300,1000)", class = "Intercept"),
-            prior("normal(0,150)", class = "b"),  
-            prior("normal(0,200)", class = "sd"),    
-            prior("normal(0,500)", class = "sigma"))
+# Prior 1 to 3 --> less informative to more informative
 
-prior2 <- c(prior("normal(300,1000)", class = "Intercept"),
-            prior("normal(0,100)", class = "b"),  
-            prior("normal(0,200)", class = "sd"),
-            prior("normal(0,500)", class = "sigma"))
-
-prior3 <- c(prior("normal(300,1000)", class = "Intercept"),
-            prior("normal(0,100)", class = "b"),  
-            prior("normal(0,150)", class = "sd"),
-            prior("normal(0,300)", class = "sigma"))
+if(prior_type == 'prior1'){
+  curr_prior = c(prior("normal(300,1000)", class = "Intercept"),
+                 prior("normal(0,150)", class = "b"),  
+                 prior("normal(0,200)", class = "sd"),    
+                 prior("normal(0,500)", class = "sigma"))
+}else if(prior_type == 'prior2'){
+  curr_prior = c(prior("normal(300,1000)", class = "Intercept"),
+                 prior("normal(0,100)", class = "b"),  
+                 prior("normal(0,200)", class = "sd"),
+                 prior("normal(0,500)", class = "sigma"))
+}else if(prior_type == 'prior3'){
+  curr_prior =c(prior("normal(300,1000)", class = "Intercept"),
+                prior("normal(0,100)", class = "b"),  
+                prior("normal(0,150)", class = "sd"),
+                prior("normal(0,300)", class = "sigma"))
+}else{
+  print('ENTER A VALID PRIOR')
+}
 
 
 get_brms_parameters <- function(prior_type){
-  curr_prior <- ifelse(prior_type == 'prior1', prior1,
-                  ifelse(prior_type == 'prior2', prior2, prior3))
-  
   parms <- list(prior = curr_prior,
                 ncores = 4,
                 niters = 12000,
